4f410bae914de9374b7cbc50625cc8c55dea1c26
Hooman <Hooman1130@users.noreply.github.com>
2021-02-27 00:57:39 -0500

    twt

--- sources/js/socials.js
+++ sources/js/socials.js
@@ -6,8 +6,12 @@ function reveal(site) {
     if (site=="yt") {
         revealOne("yt", "https://www.youtube.com/channel/UClvw_JZpf8KSN-8AMnuKJgQ");
     }
+    else if (site=="twt") {
+        revealOne("twt", "https://twitter.com/hoomancompooper");
+    }
     else if (site=="all") {
         reveal("yt");
+        reveal("twt");
     }
     else {
         console.log("Something went wrong...")
